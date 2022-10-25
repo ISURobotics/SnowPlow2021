6281fd05915d8bfa71790dcbbb474460b1cc427e
Jaden Forde <jmforde@iastate.edu>
2022-10-24 20:21:59 -0500

    fixed some bugs but not all

--- src/Path_Executor.py
+++ src/Path_Executor.py
@@ -3,13 +3,13 @@ class Path_Executor:
         self.mover = mover
         self.lidar = lidar
         self.path = [
-            lambda pose: mover.moveForward(pose, 5), 
-            lambda pose: mover.moveBackward(pose, 5)
+            lambda p_lidar: mover.move_forward(p_lidar, 0.5), 
+            lambda p_lidar: mover.move_backward(p_lidar, 0.5)
         ]
         self.current_step = 0
 
     def apply_next_action(self):
         pose = self.lidar.get_pose()
-        self.path[self.current_step](pose)
+        self.path[self.current_step](self.lidar)
         self.current_step += 1
         return self.current_step >= len(self.path)
