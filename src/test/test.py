08d5c6e8ffc2f80788a26181401e7ee2c6e3b884
Jaden Forde <jmforde@iastate.edu>
2022-10-31 20:17:36 -0500

    updated motor control ino and test.py

--- src/test/test.py
+++ src/test/test.py
@@ -1,21 +1,39 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import Int32
+from std_msgs.msg import Int16MultiArray
 
-def testingLight():
-    pub = rospy.Publisher('/light', Int32, queue_size=10)
+
+'''
+Publishes the motor values 
+
+
+motor values 
+'''
+def motorPublisher():
+    #creates a publisher that publishes to the topic 'motorSpeed' with data type of Int16MultiArray
+    pub = rospy.Publisher('/motorSpeed', Int16MultiArray, queue_size=10)
+    #creates a node for this code called pynode
     rospy.init_node('py_node', anonymous=False)
+    #sets the rate to publish at 
     rate=rospy.Rate(10)
+    #array to be published
+    arr = Int16MultiArray()
+    arr.data = []
+
     while not rospy.is_shutdown():
-        val = input("Enter value: ")
-        var = int(val)
-        pub.publish(var)
-        rospy.loginfo(var)
+        motor1 = input("Enter first value: ")
+        motor2 = input("Enter second value: ")
+        motor1 = int(val1)
+        motor2 = int(val2)
+
+        arr.data = [motor1, motor2]
+        pub.publish(arr)
+        rospy.loginfo(arr)
         rate.sleep()
 
 if __name__ == '__main__':
     try:
-        testingLight()
+        motorPublisher()
     except rospy.ROSInterruptException:
         pass
