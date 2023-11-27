e06f76609ef522d4d308e214621e03edd88cd251
Sean Frett <smf0x10dev@gmail.com>
2023-11-26 19:25:00 -0600

    Added initialization script that skips the lidar step

---
+++ test_init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# initializes ROS and arduino serial. We might need more than one arduino serial line in here
+source /home/karterk/catkin_ws/install/setup.bash
+
+# run core ros service
+roscore &
+
+echo "Waiting on roscore"
+sleep 2
+# wait for roscore to complete
+while ! rostopic list | grep "^/rosout$" >/dev/null; do
+	echo -n "."	
+	sleep 2
+done
+
+echo "roscore started. Starting arduino serial node..."
+
+# boot up the rosserial node for arduino communication
+rosrun rosserial_python serial_node.py /dev/ttyACM0 &
+echo "$!" > ~/ino_pid.txt 
+
+sleep 5
+
+
+echo "All done!"
