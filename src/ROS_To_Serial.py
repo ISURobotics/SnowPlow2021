64e4e193077f4ee62a3aab1e2909f98c8ac46e6c
TonyBaston <tony.baston@gmail.com>
2024-11-05 15:26:43 -0600

    Updated Documentation

--- src/ROS_To_Serial.py
+++ src/ROS_To_Serial.py
@@ -3,31 +3,32 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int8
 
-class DataListener(Node):
+class MotorROSListener(Node):
 
     def __init__(self):
         super().__init__("node_test")
         self.counter_ = 0
         self.left_speed=0
         self.right_speed=0
-        # create_subscriber needs 3 parameters: Msg Type, topic name, the callback, and queue size buffer
         self.left_subscriber = self.create_subscription(Int8, "/left_motor/speed", self.left_callback, 10)
         self.right_subscriber = self.create_subscription(Int8, "/right_motor/speed", self.right_callback, 10)
         self.ser = serial.Serial('/dev/ttyACM0', 115200)
-
+    #called when ROS updates the topic for the left motor
     def left_callback(self, val):
         self.left_speed=val
         self.update_serial()
+    #called when ROS updates the topic for the right motor
     def right_callback(self, val):
         self.right_speed=val
         self.update_serial()
+    #takes the stored values for the right and left motor speeds and outputs that to the Serial
     def update_serial(self):
         self.ser.write((str(self.left_speed)+"|"+str(self.right_speed)).encode('utf-8'))
         print(str(self.left_speed)+"|"+str(self.right_speed))
 
 def main(args=None):
     rclpy.init(args=args)
-    node = DataListener()
+    node = MotorROSListener()
     rclpy.spin(node)
     rclpy.shutdown()
 
