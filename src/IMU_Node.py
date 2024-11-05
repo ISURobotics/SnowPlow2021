50cce3fd23a51feae54cfa371119b4b490b89be4
SnowClone2 <splow1858@gmail.com>
2024-11-04 20:03:49 -0600

    added gps tests

---
+++ src/IMU_Node.py
@@ -0,0 +1,14 @@
+import serial
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float32MultiArray
+
+class IMUNode(Node):
+    def __init__(self):
+        self.imu_orientation_euler = [0, 0, 0]
+        self.orientation_pub = self.create_publisher(Float32MultiArray, "/imu/euler_rotation", 10)
+        self.ser = serial.Serial('/dev/ttyACM0', 115200) # May need to update
+
+    def serial_loop():
+        while True:
+            data = self.ser.read()
