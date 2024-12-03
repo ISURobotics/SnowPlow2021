866b10cb358f6b1857fcb2959f20ddec8b4d0d5f
Ryan Madigan <80542718+800472@users.noreply.github.com>
2024-12-02 19:27:43 -0600

    Update IMU_Node.py
    Both IMU receiving and motor controller commands in one node

--- src/IMU_Node.py
+++ src/IMU_Node.py
@@ -1,33 +1,46 @@
 import serial
 import rclpy
 from rclpy.node import Node
+from std_msgs.msg import Int8
 from std_msgs.msg import Float32MultiArray
+from serial import SerialException
+class SerialToROSCommunication(Node):
 
-class IMUNode(Node):
-    """
-        This class is a ROS node that handles communications with the IMU Arduino.
-        After initializing it, run the serial_loop function to make it read data from the Arduino
-        and publish it to the ROS topics /imu/magnetometer and /imu/euler_rotation.
-    """
     def __init__(self):
         super().__init__("imu_node")
+        self.counter_ = 0
+        self.left_speed=0
+        self.right_speed=0
+        self.left_subscriber = self.create_subscription(Int8, "/left_motor/speed", self.left_callback, 10)
+        self.right_subscriber = self.create_subscription(Int8, "/right_motor/speed", self.right_callback, 10)
         self.imu_orientation_euler = [0, 0, 0]
         self.imu_magnetometer = [0, 0, 0]
         self.magnetometer_pub = self.create_publisher(Float32MultiArray, "/imu/magnetometer", 10)
         self.orientation_pub = self.create_publisher(Float32MultiArray, "/imu/euler_rotation", 10)
-        self.ser = serial.Serial('/dev/ttyACM1', 115200) # May need to update
+        self.timer = self.create_timer(1/10, self.timer_callback)
         self.working_data = ""
         self.data_started = False
-
-    def serial_loop(self):
+        try:
+            self.ser = serial.Serial('/dev/ttyACM0', baudrate=115200)
+        except SerialException:
+            self.ser=serial.Serial('/dev/ttyACM1',baudrate=115200)
+    #called when ROS updates the topic for the left motor
+    def left_callback(self, val):
+        self.left_speed=val.data
+        self.update_serial()
+    #called when ROS updates the topic for the right motor
+    def right_callback(self, val):
+        self.right_speed=val.data
+        self.update_serial()
+    def timer_callback(self):
         while True:
             newest_byte = self.ser.read(1)
             if (newest_byte is None or newest_byte == b"\r"):
-                continue
+                break
             if (newest_byte == b"\n"):
                 if not self.data_started:
                     self.data_started = True
-                    continue
+                    break
                 print(self.working_data)
                 datas = self.working_data.split(";")
                 self.imu_orientation_euler = [float(c) for c in datas[0].split(",")]
@@ -43,8 +56,15 @@ class IMUNode(Node):
                 self.working_data = ""
             elif self.data_started:
                 self.working_data += newest_byte.decode("utf-8")
+    #takes the stored values for the right and left motor speeds and outputs that to the Serial
+    def update_serial(self):
+        self.ser.write((str(self.left_speed)+"|"+str(self.right_speed)+"\n").encode('utf-8'))
+        print(str(self.left_speed)+"|"+str(self.right_speed))
+def main(args=None):
+    rclpy.init(args=args)
+    node = SerialToROSCommunication()
+    rclpy.spin(node)
+    rclpy.shutdown()
 
 if __name__ == "__main__":
-    rclpy.init()
-    node = IMUNode()
-    node.serial_loop()
+    main()
