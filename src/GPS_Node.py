42f248214e498e314bacc45e69deb10636711060
Ryan Madigan <80542718+800472@users.noreply.github.com>
2025-03-03 19:58:29 -0600

    Update GPS_Node.py with better documentation

--- src/GPS_Node.py
+++ src/GPS_Node.py
@@ -39,6 +39,13 @@
 
 
 
+
+# One problem that is often had is making sure the serial port is correct. This can be changed in the constructor of the GPS class on the "self.port"
+# line. The port should only really change by its last number, and should really only ever be 0, 1, or 2 (ex. '/dev/ttyACM1')
+
+
+
+
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import PoseStamped
@@ -54,22 +61,22 @@ class GPS(Node):
 
     def __init__(self):
         super().__init__("gps_node")
-        self.port = serial.Serial('/dev/ttyACM0', baudrate=9600, timeout=1)
-        self.gps = UbloxGps(self.port)
-        self.set_output_rate(10)
-        self.origin = [0, 0]
-        self.x = 0.0
-        self.y = 0.0
-        self.calibrated = False
-        self.imu_orientation = 0.0
+        self.port = serial.Serial('/dev/ttyACM0', baudrate=9600, timeout=1)                                     # serial port to talk to GPS over USB cord
+        self.gps = UbloxGps(self.port)                                                                          # initializes GPS using Ublox's package
+        self.set_output_rate(10)                                                                                # sets the output rate of the GPS to 10 Hz
+        self.origin = [0, 0]                                                                                    # where the snowplow started relative to 0, 0 on the longitude latitude scale
+        self.x = 0.0                                                                                            # x position relative to origin
+        self.y = 0.0                                                                                            # y positioin relative to origin
+        self.calibrated = False                                                                                 # True if IMU has undergone its own internal calibration
+        self.imu_orientation = 0.0                                                                              # Orientation of the imu's compass                                                                            
         self.cal_orientations = []
         self.starting_orientation = 0.0
-        self.lat_factor = 0.0
-        self.lon_factor = 0.0
-        self.imu_sub = self.create_subscription(Float32MultiArray, '/imu/magnetometer', self.imu_callback, 10)
-        self.gps_pose_pub = self.create_publisher(PoseStamped, '/gps_pose', 10)
-        pub_rate = 20
-        self.timer = self.create_timer(1/pub_rate, self.publish_pose)
+        self.lat_factor = 0.0                                                                                   # when multiplied by the latitude, gives the position in meters relative to 0, 0 on the latitude longitude scale (somewhere in Africa)
+        self.lon_factor = 0.0                                                                                   # # when multiplied by the longitude, gives the position in meters relative to 0, 0 on the latitude longitude scale (somewhere in Africa)
+        self.imu_sub = self.create_subscription(Float32MultiArray, '/imu/magnetometer', self.imu_callback, 10)  # initializes subscription to imu's data
+        self.gps_pose_pub = self.create_publisher(PoseStamped, '/gps_pose', 10)                                 # initialized publisher for GPS's data
+        pub_rate = 20                                                                                           # how fast the GPS data is published
+        self.timer = self.create_timer(1/pub_rate, self.publish_pose)                                           # initialized timer to publish at pub_rate 
 
 
 
@@ -78,7 +85,7 @@ class GPS(Node):
 
 
     
-
+    # when the robot is where you want the (0, 0) position to be, this method is called in order to help create a local frame of reference later on
     def set_origin(self):
         global_x, global_y = self.get_global_x_y(True)
         self.origin = [global_x, global_y]
@@ -86,10 +93,14 @@ class GPS(Node):
 
 
 
-
+    # @params  setting_origin - True if you are wanting this call to set your current location as the origin
+    #
+    # Gets and returns the raw latitude and longitude data from the GPS
+    # Additionally sets the lattitude and longitude factor if necessary(explained in constuctor)
     def get_lat_lon(self, setting_origin = False):
-        coords = self.gps.geo_coords()
+        coords = self.gps.geo_coords()      # gets the raw latitude and longitude data
         if (setting_origin):
+            # these latitude and longitude factors are equations based off of this wikipedia article (https://en.wikipedia.org/wiki/Geographic_coordinate_system)
             self.lat_factor = 111132.92 - 559.82*math.cos(2*math.radians(coords.lat)) + 1.175*math.cos(4*math.radians(coords.lat)) - 0.0023*math.cos(6*math.radians(coords.lat))
             self.lon_factor = 111412*math.cos(math.radians(coords.lat)) - 93.5*math.cos(3*math.radians(coords.lat)) + 0.118*math.cos(5*math.radians(coords.lat))
         if (coords is None):
@@ -99,35 +110,42 @@ class GPS(Node):
 
 
 
-
+    # @ param  rate - the rate at which you wish the GPS to output data in Hz
+    #
+    # Sets how fast the GPS module will send data in terms of Hz
+    # MUST BE BETWEEN 1 AND 20 HZ
     def set_output_rate(self, rate: int):
 
         assert rate >= 1 and rate <= 20
         rate_ms = int(1000 / rate)
 
+        # message associated with changing the rate on the GPS
         msg = bytearray([0xB5, 0x62, 0x06, 0x08, 0x06, 0x00, (rate_ms & 0xFF),
                         (rate_ms >> 8), 0x01, 0x00, 0x00, 0x00])
-        ck_a = 0
-        ck_b = 0
+        ck_a = 0    # check byte a
+        ck_b = 0    # check byte b
+        # calculates the check bytes
         for byte in msg[2:]:
             ck_a = (ck_a + byte) & 0xFF
             ck_b = (ck_b + ck_a) & 0xFF
 
-        msg.extend([ck_a, ck_b])
-        self.port.write(msg)
+        msg.extend([ck_a, ck_b])    # adds check bytes to the message
+        self.port.write(msg)        # sends the message to the GPS module
 
 
 
 
 
-
+    # @param  setting_origin - True if you are wanting this call to set your current location as the origin
+    #
+    # Gets and returns the global x and y position relative to the (0, 0) point on the latitude longitude scale (somewhere near Africa)
     def get_global_x_y(self, setting_origin = False):
         lat, lon = self.get_lat_lon(setting_origin)
 
         
 
-        global_y = self.lat_factor * lat
-        global_x = self.lon_factor * lon
+        global_y = self.lat_factor * lat    # converts latitude degrees to meters
+        global_x = self.lon_factor * lon    # converts longitude degrees to meters
 
         return global_x, global_y
 
@@ -137,38 +155,41 @@ class GPS(Node):
 
 
 
-
+    # Gets and returns the local x y positioning which is relative to the origin you set and factors in your starting orientation as well by rotating the x y axes
+    # to match how the snowplow was oriented when the origin was set
     def get_local_x_y(self):
-        global_x, global_y = self.get_global_x_y()
-        local_x = global_x - self.origin[0]
-        local_y = global_y - self.origin[1]
-        local_x, local_y = self.rotate_axis(local_x, local_y, math.radians(self.starting_orientation))
+        global_x, global_y = self.get_global_x_y()                                                      # gets global positioning in x, y coordinates
+        local_x = global_x - self.origin[0]                                                             # translates x positioning to local positioning relative to origin
+        local_y = global_y - self.origin[1]                                                             # translates y positioning to local positioning relative to origin
+        local_x, local_y = self.rotate_axis(local_x, local_y, math.radians(self.starting_orientation))  # rotates the x and y positioning to match orienation the snowplow was in when the origin was set
 
         return local_x, local_y
 
 
-
+    # @param  x - original x position, with a x y axes where y is due north and x is due east
+    # @param  y - original y position, with a x y axes where y is due north and x is due east
+    # @param  theta - orientation you wish to rotate the x y axes by in radians with a counterclockwise rotation being positive
     def rotate_axis(self, x, y, theta):
-        x_prime = x*math.cos(theta) - y*math.sin(theta)
-        y_prime = x*math.sin(theta) + y*math.cos(theta)
+        x_prime = x*math.cos(theta) - y*math.sin(theta) 
+        y_prime = x*math.sin(theta) + y*math.cos(theta) 
         return x_prime, y_prime
 
 
 
-
+    # closes serial port for GPS
     def close_port(self):
         self.port.close()
 
 
 
-
+    # Callback function for IMU subscriber which just gets the IMU data
     def imu_callback(self, msg):
         self.imu_orientation = self.angle_conversion(msg.data[2])
         if (msg.data[0] == 0.0):
             self.calibrated = True
 
 
-
+    # The message type that the GPS node of ROS will send out is a PoseStamped message, so this method simply arranges all the data in that particular message format
     def create_pose(self):
         msg = PoseStamped()
 
@@ -190,22 +211,27 @@ class GPS(Node):
 
 
 
-
+    # Publish method for the GPS publisher which publishes the GPS's orientation in a PoseStamped format
     def publish_pose(self):
         msg = self.create_pose()
         self.gps_pose_pub.publish(msg)
 
-    
+    # Converts the angle given by the IMU to the frame of 0 degrees being the snowplow facing directly north and counterclockwise rotation being a positive rotation
     def angle_conversion(self, angle):
         return ((-angle + 360)%360)
 
 
-
+    # The IMU needs to be calibrated properly before it can send somewhat accurate compass data. To calibrate it, it needs to be moved around  and rotated around
+    # in all directions, usually done by a figure 8 motion.
+    # Additionally, since the IMU's compasses data is a little noisy, this function averages 20 data points taken to try and acheive a more accurate reading
     def calibrate_orientation(self):
+        # if there have been 20 data points taken, average them and set that to be the starting orientation of the snowplow
         if (len(self.cal_orientations) > 20):
             self.starting_orientation = (sum(self.cal_orientations) / len(self.cal_orientations))
             self.cal_orientations = []
             return True
+        # otherwise, if the IMU's magnetometer isn't calibrated yet, wait
+        # if it is calibrated, add the latest data point to the array that will be averaged to find starting orienation
         else:
             if (not self.calibrated):
                 print("Calibrating Orientation...")
@@ -254,14 +280,14 @@ class GPS(Node):
 
 
 def main(args=None):
-    rclpy.init(args=args)
-    gps = GPS()
+    rclpy.init(args=args)                       # initialize ROS2
+    gps = GPS()                                 # create GPS object
     #while(not gps.calibrate_orientation()):
     #    rclpy.spin_once(gps)
-    gps.set_output_rate(10)
+    gps.set_output_rate(10)                     # set output rate of GPS to 10 Hz
     print("Done Calibrating IMU")
-    gps.set_origin()
-    rclpy.spin(gps)
+    gps.set_origin()                            # set the origin an starting orientation to whereever the Snowplow is and the direction it is facing
+    rclpy.spin(gps)                        
     gps.destroy_node()
     rclpy.shutdown()
 
